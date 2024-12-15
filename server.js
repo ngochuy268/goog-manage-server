d4404f8661b9aae8b6d275251ce6a344390dc480
ngochuy268 <huyduclong2642000@gmail.com>
2024-12-15 08:24:14 +0700

    first

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5000,
     connectionLimit: 5,
     waitForConnections: true,
     queueLimit: 0,
